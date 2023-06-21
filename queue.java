e0aee6bf83437bc314e209d81cd5142084ceafe7
Jamshadj <jamshadalij2004@gmail.com>
2023-06-21 09:46:23 +0530

    Creating queue data stracture

--- queue.java
+++ queue.java
@@ -11,15 +11,7 @@ public class queue {
     public Node front;
     public Node rear;
 
-    public void enqueue(int element){
-        Node newNode=new Node(element);
-        if(rear==null){
-            front=rear=newNode;
-            return;
-        }
-        rear.next=newNode;
-        rear=newNode;
-    }
+
 
     public void display(){
         Node temp=front;
