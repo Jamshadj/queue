6845090d63baf79f01f0f7a065b68bc72e3c8167
Jamshadj <jamshadalij2004@gmail.com>
2023-06-21 09:47:28 +0530

    enqueue

--- queue.java
+++ queue.java
@@ -33,7 +33,13 @@ public class queue {
         }
     }
 
-
+    public void dequeue(){
+        if(rear==null){
+            System.out.println("empty");
+        }else{
+            front=front.next;
+        }
+    }
 
     public static void main(String[] args) {
         queue list =new queue();
